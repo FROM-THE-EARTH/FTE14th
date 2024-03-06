949cbce4a475c41424088d8d1f754291541d609d
karisora <142006610+karisora@users.noreply.github.com>
2024-03-06 14:40:14 +0900

    Update gps_test.py

--- NiCs2024/AutomaticController/test/gps_test.py
+++ NiCs2024/AutomaticController/test/gps_test.py
@@ -1,28 +1,27 @@
+# from curses import echo
 import serial
-from micropyGPS import MicropyGPS
 import threading
 import time
+import RPi.GPIO as GPIO
+from library.micropyGPS import micropyGPS
 
-gps = MicropyGPS(9, 'dd') # MicroGPSオブジェクトを生成する。
-                                     # 引数はタイムゾーンの時差と出力フォーマット
 
-def rungps(): # GPSモジュールを読み、GPSオブジェクトを更新する
+
+def GPS_test():  # GPSモジュールを読み、GPSオブジェクトを更新する
+    global lat
+    global lng
     s = serial.Serial('/dev/serial0', 115200, timeout=10)
-    s.readline() # 最初の1行は中途半端なデーターが読めることがあるので、捨てる
+    s.readline()  # 最初の1行は中途半端なデーターが読めることがあるので、捨てる
     while True:
-        sentence = s.readline().decode('utf-8') # GPSデーターを読み、文字列に変換する
-        if sentence[0] != '$': # 先頭が'$'でなければ捨てる
+        sentence = s.readline().decode('unicode_escape')  # GPSデーターを読み、文字列に変換する
+        if sentence[0] != '$':  # 先頭が'$'でなければ捨てる
             continue
-        for x in sentence: # 読んだ文字列を解析してGPSオブジェクトにデーターを追加、更新する
+        for x in sentence:  # 読んだ文字列を解析してGPSオブジェクトにデーターを追加、更新する
             gps.update(x)
-
-gpsthread = threading.Thread(target=rungps, args=()) # 上の関数を実行するスレッドを生成
-gpsthread.daemon = True
-gpsthread.start() # スレッドを起動
-
-while True:
-    h = gps.timestamp[0] if gps.timestamp[0] < 24 else gps.timestamp[0] - 24
-    print('%02d:%02d:%04.1f' % (h, gps.timestamp[1], gps.timestamp[2]))
-    print('lat= %2.8f, lng= %2.8f, alt= %f' % (gps.latitude[0], gps.longitude[0], gps.altitude))
-    print('')
-    time.sleep(1.0)
+        lat = gps.latitude[0]
+        lng = gps.longitude[0]
+        print(lat)
+        print(lng'/n')
+        if mainProgramFlag == False:
+            break
+    print('setGpsData()T1: Closed')
